424f38942e2e7cc5e5202266b452cb0aedf6b5f3
SNMiroshnichenko <miroshnichenkos@mail.ru>
2026-02-12 16:13:49 +0300

    first

---
+++ src/test/java/ru/job4j/assertj/BoxTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BoxTest {
+    @Test
+    void isThisSphere() {
+        Box box = new Box(0, 10);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Sphere");
+    }
+
+    @Test
+    void isThisTetrahedron() {
+        Box box = new Box(4, 6);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Tetrahedron");
+    }
+
+
+
+}
