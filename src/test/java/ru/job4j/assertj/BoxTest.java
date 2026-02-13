0233a4de8642a699661b96b01b3b040b20d9a7f9
SNMiroshnichenko <miroshnichenkos@mail.ru>
2026-02-13 16:01:02 +0300

    3. Утверждения с примитивными типами [#504883]

--- src/test/java/ru/job4j/assertj/BoxTest.java
+++ src/test/java/ru/job4j/assertj/BoxTest.java
@@ -19,6 +19,45 @@ class BoxTest {
         assertThat(name).isEqualTo("Tetrahedron");
     }
 
+    @Test
+    void isThisSphereHasZeroVertex() {
+        Box box = new Box(0, 10);
+        int vertex = box.getNumberOfVertices();
+        assertThat(vertex).isZero();
+    }
 
+    @Test
+    void isThisTetrahedronHasFourVertices() {
+        Box box = new Box(4, 6);
+        int vertices = box.getNumberOfVertices();
+        assertThat(vertices).isEqualTo(4);
+    }
 
+    @Test
+    void isThisUnknownExist() {
+        Box box = new Box(3, 8);
+        boolean exist = box.isExist();
+        assertThat(exist).isFalse();
+    }
+
+    @Test
+    void isThisCubeExist() {
+        Box box = new Box(8, 12);
+        boolean exist = box.isExist();
+        assertThat(exist).isTrue();
+    }
+
+    @Test
+    void isThisCubeAreaEqual_6() {
+        Box box = new Box(8, 1);
+        double area = box.getArea();
+        assertThat(area).isEqualTo(6);
+    }
+
+    @Test
+    void isThisSphereAreaEqual_4piR2() {
+        Box box = new Box(0, 1);
+        double area = box.getArea();
+        assertThat(area).isEqualTo(4 * Math.PI);
+    }
 }
